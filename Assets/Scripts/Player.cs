69d3bb7c48abeb00a6226bd495648a7b5099a793
Alinto <AntonLisborg@gmail.com>
2024-05-29 11:16:01 +0200

    efwefwegwg

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpHeight = 5f;
     [SerializeField] int amountOfJumps = 2;
-
-    Vector2 moveInput;
-    Vector2 jumpInput;
 
     Rigidbody2D myRigidbody;
 
@@ -21,18 +17,13 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        Run();
+
     }
 
-    void OnMove(InputValue value)
+    void OnMove()
     {
-        moveInput = value.Get<Vector2>();
-    }
 
-    void Run()
-    {
-        myRigidbody.velocity = new Vector2(moveInput.x * moveSpeed, jumpInput.y * jumpHeight);
     }
 }
