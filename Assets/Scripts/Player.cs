4fd33b2331c5cba2b49faa17ef04c0a552fa30e9
Andreas <andafrana@gmail.com>
2024-05-29 11:02:12 +0200

    The thingamajig is moving

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpHeight = 5f;
     [SerializeField] int amountOfJumps = 2;
+
+    Vector2 moveInput;
+    Vector2 jumpInput;
 
     Rigidbody2D myRigidbody;
 
@@ -17,13 +21,18 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+        Run();
     }
 
-    void OnMove()
+    void OnMove(InputValue value)
     {
+        moveInput = value.Get<Vector2>();
+    }
 
+    void Run()
+    {
+        myRigidbody.velocity = new Vector2(moveInput.x * moveSpeed, jumpInput.y * jumpHeight);
     }
 }
