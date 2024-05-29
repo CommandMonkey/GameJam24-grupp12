59d109cdce8b447f8a0ca17ca19cc7dd1d7b65f2
Nord <tage.walder14@gmail.com>
2024-05-29 18:26:43 +0200

    New Door

---
+++ Assets/Scripts/Puzle/InteractablePuzzle.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablePuzzle : MonoBehaviour
+{
+    protected bool isActive; 
+    public bool CheckIfActiv { get { return isActive; }   }
+}
---
+++ Assets/Scripts/Puzle/door.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    [SerializeField] float howLongTostayOpen;
+    float timeUntilClosed;
+    [SerializeField] float doorOpeningSpeed;
+    [SerializeField] float doorClosingSpeed;
+
+    [SerializeField] Transform posToMoveTo;
+
+    [Tooltip("The target or button this door is coneected to")]
+    [SerializeField] InteractablePuzzle ConnectedTo;
+
+    Vector2 startPos;
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+    private void Update()
+    {
+        if (ConnectedTo.CheckIfActiv)
+            OpenDoor();
+        else
+            CloseDoor();
+    }
+
+    private void CloseDoor()
+    {
+        if(timeUntilClosed <= 0) 
+        MoveDoor(startPos, doorClosingSpeed);
+        else 
+        timeUntilClosed -= Time.deltaTime;
+    }
+
+    void OpenDoor()
+    {
+        timeUntilClosed = howLongTostayOpen;
+        MoveDoor(posToMoveTo.position, doorOpeningSpeed);
+    }
+    void MoveDoor(Vector2 moveTo, float Speed)
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveTo, Speed * Time.deltaTime);
+    }
+}
