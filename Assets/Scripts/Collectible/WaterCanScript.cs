95579e6ee031daff6d3aab7dffc03dfd287a9e69
Felix Sporrong Carlander <felix.sporrongcarla@learnet.se>
2024-05-29 13:05:30 +0200

    min

--- Assets/Scripts/Collectible/WaterCanScript.cs
+++ Assets/Scripts/Collectible/WaterCanScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WaterCanScript : Collectible
 {
-    bool WaterCan;
+    public bool WaterCan;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Collect();
