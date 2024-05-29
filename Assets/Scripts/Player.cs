e5d941aa12d44ff7a1b5accd16596b95c1c43620
Andreas <andafrana@gmail.com>
2024-05-29 10:49:59 +0200

    Input System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpHeight = 5f;
+    [SerializeField] int amountOfJumps = 2;
+
+    Rigidbody2D myRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnMove()
+    {
+
     }
 }
